72a5aa390c5e51cd4af033f73e5a85e29fc0d754
Luan <luan086@gmail.com>
2024-05-28 22:11:02 +0200

    Created a delete book button

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = random5Digits();
 };
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+};
+
+function random5Digits() {
+   return Math.random().toString(36).substring(3,9);
 };
 
 const hobbit = new Book("Hobbit", "J.R.R. Tolkien", "265 pages", "not read yet");
@@ -23,29 +28,88 @@ addBookToLibrary(lotr1);
 addBookToLibrary(lotr2);
 addBookToLibrary(lotr3);
 
-const main_container = document.querySelector(".main");
-for (const book in myLibrary) {
-    console.log(myLibrary[book].title);
+const dialog = document.querySelector(".dialog-new-book");
+const newBook = document.querySelector(".new-book");
+newBook.addEventListener("click", (e) => {
+    dialog.showModal();
+});
 
+const cancelBtn = document.querySelector(".cancel-button");
+cancelBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+});
+
+const confirmBtn = document.querySelector(".confirm-button");
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const formTitle = document.querySelector(".form-title");
+    const formAuthor = document.querySelector(".form-author");
+    const formPages = document.querySelector(".form-pages");
+    const formRead = document.querySelector(".form-read");
+
+    addBookToLibrary(
+        new Book(formTitle.value, formAuthor.value, formPages.value, formRead.value)
+    );
+
+    createCard(myLibrary[myLibrary.length - 1]);
+
+    formTitle.value = "";
+    formAuthor.value = "";
+    formPages.value = "";
+    formRead.value = "";
+
+    dialog.close();
+});
+
+function createCard(book) {
     let card = document.createElement("div");
     card.className = "book";
+    card.id = book.id;
 
     let card_title = document.createElement("h2");
     card_title.className = "title";
-    card_title.textContent = myLibrary[book].title;
+    card_title.textContent = book.title;
     card.appendChild(card_title);
 
     let card_author = document.createElement("p");
-    card_author.textContent = myLibrary[book].author;
+    card_author.textContent = book.author;
     card.appendChild(card_author);
 
     let card_pages = document.createElement("p");
-    card_pages.textContent = myLibrary[book].pages;
+    card_pages.textContent = book.pages;
     card.appendChild(card_pages);
 
     let card_read = document.createElement("p");
-    card_read.textContent = myLibrary[book].read;
+    card_read.textContent = book.read;
     card.appendChild(card_read);
 
-    main_container.appendChild(card);
+    let card_btn_delete = document.createElement("button");
+    card_btn_delete.className = "card-btn-delete";
+    card_btn_delete.id = book.id;
+    card_btn_delete.textContent = "Delete";
+    card.appendChild(card_btn_delete);
+
+    main.appendChild(card);
+};
+
+function deleteBook() {
+    console.log("hello world");
 }
+
+const main = document.querySelector(".main");
+for (const book in myLibrary) {
+    createCard(myLibrary[book]);
+}
+
+main.addEventListener("click", (e) => {
+    if(e.target.tagName === 'BUTTON') {
+        const book_id = e.target.id;
+        const index = myLibrary.findIndex(x => x.id === book_id);
+        
+        myLibrary.splice(index, 1);
+
+        const book_card = document.querySelector(`#${book_id}`);
+        book_card.parentNode.removeChild(book_card);
+    }
+});
